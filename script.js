f0677377c01bf7563f245d09550de8da71b656cd
locb65 <locb65@gmail.com>
2022-12-10 15:41:09 -0500

    add shuffle function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Part: create a working deck on cards in an array
+//Part2: create a working deck on cards in an array
 
 const deck = deckOfCards();
 
@@ -15,3 +15,20 @@ const deck = deckOfCards();
     return deck;
  }
 console.log(deck)
+
+//Part2: Shuffle the deck
+//shuffles the deck by picking 2 cards and shuffling those two cards
+//this is repeated over 10000 repetitions
+
+function shuffle(deck) {
+  for (let i = 0; i < 10000; i++) {
+    let card1 = Math.floor((Math.random() * deck.length));
+    let card2 = Math.floor((Math.random() * deck.length));
+    let tmp = deck[card1];
+
+    deck[card1] = deck[card2];
+    deck[card2] = tmp;
+  }
+} 
+
+shuffle(deck)
